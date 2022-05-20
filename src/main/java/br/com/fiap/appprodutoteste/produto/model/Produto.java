c8e1b7a4fd0f5a21372adf1630e713181969ea2d
José Romualdo Da Costa <=>
2022-05-19 21:32:59 -0300

    add one-to-one mapping

--- src/main/java/br/com/fiap/appprodutoteste/produto/model/Produto.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/model/Produto.java
@@ -2,11 +2,14 @@ package br.com.fiap.appprodutoteste.produto.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 
@@ -24,6 +27,10 @@ public class Produto {
 	private String nome; 
 	private Integer quantidade; 
 	private BigDecimal valor;
+	
+	@OneToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "id_fabricante")
+	private Fabricante fabricante;
 	
 	public Produto() {
 	}
