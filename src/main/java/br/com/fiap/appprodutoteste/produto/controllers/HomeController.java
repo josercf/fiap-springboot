24696c621bbd8f1c477801f60cf787f75de9785b
José Romualdo Da Costa <=>
2022-06-01 21:03:50 -0300

    validação

--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/HomeController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/HomeController.java
@@ -4,15 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home(HttpServletRequest request) {
-		request.setAttribute("nomeUsuario", "Thiago Souza Xavier");
-		
-		return "home";
+	public ModelAndView home() {
+		ModelAndView model = new ModelAndView("redirect:/produtos");
+		return model;
 	}
 
 }
--- src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/appprodutoteste/produto/controllers/ProdutoController.java
@@ -1,18 +1,18 @@
 package br.com.fiap.appprodutoteste.produto.controllers;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
+import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.appprodutoteste.produto.dto.ProdutoDto;
-import br.com.fiap.appprodutoteste.produto.model.Categoria;
 import br.com.fiap.appprodutoteste.produto.model.Produto;
 import br.com.fiap.appprodutoteste.produto.repositories.CategoriaRepository;
 import br.com.fiap.appprodutoteste.produto.repositories.ProdutoRepository;
@@ -44,7 +44,13 @@ public class ProdutoController {
 	}
 
 	@PostMapping("produtos")
-	public ModelAndView salvar( ProdutoDto produto) {
+	public ModelAndView salvar(@Valid ProdutoDto produto, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()){
+			System.out.println("*******Erro de validação**********");
+			ModelAndView erros = new ModelAndView("/produtos/criar");
+			return erros;
+		}
 		Produto produtoEntity = modelMapper.map(produto, Produto.class);
 		
 		produtoRepository.save(produtoEntity);
